ab3e50a1c0dfb7abfa721ce061836a5553cea37f
Ltapiero <l_tapiero@hotmail.com>
2022-09-13 17:31:10 -0500

    final v6

--- src/main.js
+++ src/main.js
@@ -43,7 +43,7 @@ function createMovies(movies, container) {
 		movieImg.setAttribute("alt", movie.title);
 		movieImg.setAttribute(
 			"src",
-			"https://image.tmdb.org/t/p/w300" + movie.poster_path
+			"https://image.tmdb.org/t/p/w500" + movie.poster_path
 		);
 		movieContainer.appendChild(movieImg);
 		container.appendChild(movieContainer);
@@ -73,7 +73,7 @@ function createSeries(movies, container) {
 		movieImg.setAttribute("alt", movie.title);
 		movieImg.setAttribute(
 			"src",
-			"https://image.tmdb.org/t/p/w300" + movie.poster_path
+			"https://image.tmdb.org/t/p/w500" + movie.poster_path
 		);
 		movieContainer.appendChild(movieImg);
 		container.appendChild(movieContainer);
